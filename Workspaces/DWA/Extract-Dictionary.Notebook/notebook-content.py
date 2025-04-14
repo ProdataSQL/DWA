3fd835d25e4b86a0620d526f8f4d84e0e213f49b
Shruti Goyal <150908027+shrutiProdata@users.noreply.github.com>
2025-04-14 15:26:53 +0100

    Committing 1 item from workspace 5941a6c0-8c98-4d79-b065-a3789e9e0960

--- Workspaces/DWA/Extract-Dictionary.Notebook/notebook-content.py
+++ Workspaces/DWA/Extract-Dictionary.Notebook/notebook-content.py
@@ -34,7 +34,6 @@ import pandas as pd
 from builtin.sql_connection_helper import create_engine
 import sempy.fabric as fabric
 import re
-
 tenant_id=spark.conf.get("trident.tenant.id")
 workspace_id=spark.conf.get("trident.workspace.id")
 lakehouse_id=spark.conf.get("trident.lakehouse.id")
@@ -42,27 +41,22 @@ lakehouse_name=spark.conf.get("trident.lakehouse.name")
 sql_end_point=connection_string= fabric.FabricRestClient().get(f"/v1/workspaces/{workspace_id}/lakehouses/{lakehouse_id}").json()['properties']['sqlEndpointProperties']['connectionString']
 connection_string = "Driver={{ODBC Driver 18 for SQL Server}};Server={}".format(sql_end_point)
 pattern = '[ ,;{}()\n\t/=]'
-
 # List Datasets from meta data
 engine = create_engine(connection_string)
 with engine.connect() as alchemy_connection:  
     sql =f"select  lower(SCHEMA_NAME(schema_id) + '.' + name) as edw_object_name ,  name as edw_table_name, SCHEMA_NAME(schema_id) as schema_name, create_date, modify_date  from {edw}.sys.tables"
     df= pd.read_sql_query (sql, alchemy_connection)
     spark_df=spark.createDataFrame(df).write.mode("overwrite").saveAsTable("dict_edw_tables")
-
     sql =f"select  lower(SCHEMA_NAME(schema_id) + '.' + name) as edw_object_name ,  name as edw_table_name, SCHEMA_NAME(schema_id) as schema_name, create_date, modify_date  from {lh}.sys.tables"
     df= pd.read_sql_query (sql, alchemy_connection)
     spark_df=spark.createDataFrame(df).write.mode("overwrite").saveAsTable("dict_lh_tables")
-
 #Store Fabric Artefacts
 df=fabric.list_items()
 df=df.rename(columns=dict(zip(df.columns, [re.sub(pattern, '_', col.strip(pattern).lower()) for col in df.columns])))
 spark.createDataFrame(df).write.mode("overwrite").saveAsTable("dict_artefacts")
-
 #List Fabric Workspaces
 df_workspaces =fabric.list_workspaces()
 df_workspaces=df_workspaces[df_workspaces['Capacity Id'].notna()] 
-
 #List DataSets and Model Tables 
 df_datasets =fabric.list_datasets()
 tables=[]
@@ -75,8 +69,10 @@ for row in df_datasets.itertuples(index=True, name='datasets'):
     tables.append(df)
 df=pd.concat(tables, ignore_index=True)
 df=df.rename(columns=dict(zip(df.columns, [re.sub(pattern, '_', col.strip(pattern).lower()) for col in df.columns])))
-spark.createDataFrame(df).write.mode("overwrite").saveAsTable(f"dict_dataset_tables") 
-
+if not df.empty:
+    spark.createDataFrame(df).write.mode("overwrite").saveAsTable(f"dict_dataset_tables")
+else:
+    print("DataFrame is empty. Skipping write to Spark (dict_dataset_tables).") 
 #Store Columns for Data Dictionary
 columns=[]
 for row in df_datasets.itertuples(index=True, name='datasets'):
@@ -87,9 +83,11 @@ for row in df_datasets.itertuples(index=True, name='datasets'):
     df.insert(0, 'dataset', dataset)
     columns.append(df)
 df=pd.concat(columns, ignore_index=True)
-df = spark.createDataFrame(df)
-df.write.mode("overwrite").saveAsTable(f"dict_dataset_columns") 
-
+if not df.empty:
+    df = spark.createDataFrame(df)
+    df.write.mode("overwrite").saveAsTable(f"dict_dataset_columns") 
+else:
+    print("DataFrame is empty. Skipping write to Spark (dict_dataset_columns).")
 measures=[]
 for row in df_datasets.itertuples(index=True, name='datasets'):
     dataset = row[1]
@@ -99,8 +97,12 @@ for row in df_datasets.itertuples(index=True, name='datasets'):
     df.insert(0, 'dataset', dataset)
     measures.append(df)
 df=pd.concat(measures, ignore_index=True)
-df = spark.createDataFrame(df)
-df.write.mode("overwrite").saveAsTable(f"dict_dataset_measures") 
+if not df.empty:
+    df = spark.createDataFrame(df)
+    df.write.mode("overwrite").saveAsTable(f"dict_dataset_measures") 
+else:
+    print("DataFrame is empty. Skipping write to Spark (dict_dataset_measures).")
+
 
 
 # METADATA ********************
