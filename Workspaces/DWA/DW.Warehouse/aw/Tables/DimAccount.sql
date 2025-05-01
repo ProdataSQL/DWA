a5ccabf3877fbb54cd54ad7af6be3709f52c5738
Shruti Goyal <150908027+shrutiProdata@users.noreply.github.com>
2025-05-01 12:47:04 +0100

    Committing 2 items from workspace 5941a6c0-8c98-4d79-b065-a3789e9e0960

---
+++ Workspaces/DWA/DW.Warehouse/aw/Tables/DimAccount.sql
@@ -0,0 +1,16 @@
+CREATE TABLE [aw].[DimAccount] (
+
+	[AccountKey] varchar(16) NULL, 
+	[ParentAccountKey] varchar(16) NULL, 
+	[AccountCodeAlternateKey] int NOT NULL, 
+	[ParentAccountCodeAlternateKey] int NULL, 
+	[AccountDescription] varchar(50) NOT NULL, 
+	[AccountType] varchar(50) NULL, 
+	[Operator] varchar(50) NOT NULL, 
+	[CustomMembers] varchar(50) NULL, 
+	[ValueType] varchar(50) NOT NULL, 
+	[CustomMemberOptions] varchar(200) NULL, 
+	[RowChecksum] int NOT NULL, 
+	[FileName] varchar(512) NULL, 
+	[LineageKey] varchar(36) NOT NULL
+);
