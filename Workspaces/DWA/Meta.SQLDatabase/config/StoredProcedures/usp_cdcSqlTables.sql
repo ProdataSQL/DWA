00dcccd77418a61014aab09070d9570996fb6bce
Kristan Johnson <kghjohnson@gmail.com>
2025-08-28 16:13:32 +0100

    add SqlTables param

--- Workspaces/DWA/Meta.SQLDatabase/config/StoredProcedures/usp_cdcSqlTables.sql
+++ Workspaces/DWA/Meta.SQLDatabase/config/StoredProcedures/usp_cdcSqlTables.sql
@@ -1,21 +1,34 @@
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
 /*
     Return a List of Tables for CDC and their Primary Keys
     Used By: Extract-Parquet-CDC Notebook
     Test: 
         exec [config].[usp_cdcSqlTables] @LakehouseName='StageDM2'
     History:    15/06/2025 Bob, Created           
+                29/07/2025 Kristan, added SqlTables param
 
 */
-CREATE     PROCEDURE [config].[usp_cdcSqlTables]
+CREATE PROCEDURE [config].[usp_cdcSqlTables]
 (
-	@LakehouseName sysname ='StageDM2'
+    @LakehouseName sysname = 'FabricLH',
+    @SqlTables sysname = 'config.cdcSqlTables'
 )
 AS
 BEGIN
-    SET NOCOUNT ON
-  
-    SELECT [Table], PrimaryKeys  FROM config.cdcSqlTables where LakehouseName =@LakehouseName and Enabled=1
-END
+    SET NOCOUNT ON;
 
+    DECLARE @sql nvarchar(max)
+
+    SET @sql = 'SELECT [Table], PrimaryKeys
+        FROM ' + @SqlTables + '
+        WHERE LakehouseName = @LakehouseName AND Enabled = 1'
+
+    EXEC sp_executesql @sql,
+        N'@LakehouseName sysname',
+        @LakehouseName = @LakehouseName
+END
 GO
 
