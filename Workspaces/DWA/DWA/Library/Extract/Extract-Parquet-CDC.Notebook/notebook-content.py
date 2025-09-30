16429b829a08813d04136a734bd76310e65a9993
Bob Duffy <bob@prodata.ie>
2025-09-30 14:32:17 +0100

    Committing 1 item from workspace 5941a6c0-8c98-4d79-b065-a3789e9e0960

--- Workspaces/DWA/DWA/Library/Extract/Extract-Parquet-CDC.Notebook/notebook-content.py
+++ Workspaces/DWA/DWA/Library/Extract/Extract-Parquet-CDC.Notebook/notebook-content.py
@@ -40,7 +40,6 @@ TargetConnectionSettings = None
 ActivitySettings = None
 LineageKey = '00000000-0000-0000-0000-000000000000'
 
-
 # METADATA ********************
 
 # META {
@@ -68,29 +67,23 @@ import sempy.fabric as fabric
 from datetime import datetime
 from delta.tables import DeltaTable
 import pandas as pd
-
 activity_settings = json.loads(ActivitySettings or '{}')
-
 source_connection_settings = json.loads(SourceConnectionSettings or '{}')
 source_lakehouse_id = source_connection_settings.get("lakehouseId",fabric.get_lakehouse_id())
 source_workspace_id = source_connection_settings.get("workspaceId",fabric.get_workspace_id())
 source_lakehouse_name = source_connection_settings.get("lakehouse",fabric.resolve_item_name(item_id=source_lakehouse_id, workspace=source_workspace_id))
 source_workspace_name = fabric.resolve_workspace_name(source_workspace_id)
-
 target_connection_settings = json.loads(TargetConnectionSettings or '{}')
 target_lakehouse_id = target_connection_settings.get("lakehouseId",fabric.get_lakehouse_id())
 target_workspace_id = target_connection_settings.get("workspaceId",fabric.get_workspace_id())
 target_lakehouse_name = target_connection_settings.get("lakehouse",fabric.resolve_item_name(item_id=target_lakehouse_id, workspace=target_workspace_id))
 target_workspace_name = fabric.resolve_workspace_name(target_workspace_id)
-
 source_settings = json.loads(SourceSettings or '{}')
 cdc_sql_table = source_settings.get('sqlTables','config.cdcSqlTables')
 schema = source_settings.get('schema')
 tables = source_settings.get('sqlTables','config.cdcSqlTables')
-
 target_settings = json.loads(TargetSettings or '{}')
 commit_sproc = target_settings.get('commitSproc','config.usp_cdcSqlCommit')
-
 
 # METADATA ********************
 
@@ -111,14 +104,12 @@ if source_workspace_name==target_workspace_name:
 else:
     print(f"Source Workspace: {source_workspace_name}, Lakehouse: {source_lakehouse_name}")
     print(f"Target Workspace: {target_workspace_name}, Lakehouse: {target_lakehouse_name}")
-
 client = fabric.FabricRestClient()
 items = client.get(f"/v1/workspaces/{source_workspace_id}/SqLDatabases").json()["value"]
 sql_database=next((endpoint for endpoint in items if endpoint["displayName"] == "Meta"))
 sql_end_point = sql_database["properties"]["serverFqdn"]
 sql_database_name = sql_database["properties"]["databaseName"]
 connection_string = f"Driver={{ODBC Driver 18 for SQL Server}};Server={sql_end_point};database={sql_database_name}"
-
 spark.conf.set("spark.sql.parquet.int96RebaseModeInRead", "LEGACY") #Needed if dates below 1800
 spark.conf.set("spark.sql.parquet.datetimeRebaseModeInWrite", "LEGACY")
 files_path = f"abfss://{source_workspace_id}@onelake.dfs.fabric.microsoft.com/{source_lakehouse_id}/Files/LandingZone"
@@ -128,7 +119,6 @@ sql_connection = engine.raw_connection()
 batch_start_time = datetime.now()
 with engine.connect() as alchemy_connection:
     tables = pd.read_sql_query(f"exec config.usp_cdcSqlTables '{source_lakehouse_name}', '{cdc_sql_table}'", alchemy_connection)
-
 folders_to_process = []
 if schema:
     schema_folder = f"{schema}.schema"
@@ -138,8 +128,7 @@ if schema:
             folders_to_process.append(folder)
             break  # Found the specific schema folder
 else:
-    folders_to_process = list(notebookutils.fs.ls(files_path))
-    
+    folders_to_process = list(notebookutils.fs.ls(files_path))  
 files = []
 for folder in folders_to_process:
     for subfolder in notebookutils.fs.ls(folder.path):
@@ -147,7 +136,6 @@ for folder in folders_to_process:
             files.extend(
                 sorted([x.path for x in notebookutils.fs.ls(subfolder.path) if not x.isDir])
             )
-
 for file in files:
     t = datetime.now()
     file_path =file
@@ -156,8 +144,7 @@ for file in files:
     schema_name=file.split('/')[-3].removesuffix('.schema')
     table_df = tables[tables['Table'].str.endswith(f'.{table_name}', na=False)]
     primary_keys = table_df ['PrimaryKeys'].iloc[0] if not table_df.empty else None
-    source_table = table_df['Table'].iloc[0] if not table_df.empty else None
-        
+    source_table = table_df['Table'].iloc[0] if not table_df.empty else None   
     df = spark.read.parquet(file_path ) 
     cdc_columns = ["__$start_lsn", "__$seqval", "__$operation", "__$update_mask"]
     existing_columns = [col for col in cdc_columns if col in df.columns]
@@ -187,31 +174,23 @@ for file in files:
             sink_df.alias("target").merge(df.alias("source"), merge_condition).whenNotMatchedInsertAll().whenMatchedUpdateAll().execute()
         print(f"\t- {operation} for {source_table}=>{schema_name}.{table_name} {(datetime.now() - t).total_seconds():.1f} seconds")
     sql = f'exec {commit_sproc} @Table=?'
-    values = (tables)
-    cursor = sql_connection.cursor()
-    cursor.execute(sql, values)
-    sql_connection.commit()
     values = (f"{source_table}",)
     cursor = sql_connection.cursor()
     cursor.execute(sql, values)
     sql_connection.commit()
     notebookutils.fs.rm(file_path, recurse=False)
-
 # Remove Empty Folders - only for processed schemas
 for processed_folder in folders_to_process:
     subfolders = [f.path for f in mssparkutils.fs.ls(processed_folder.path) if f.isDir]
-    
     for folder in subfolders:
         folder_contents = mssparkutils.fs.ls(folder)
         if not folder_contents or all(item.isDir for item in folder_contents):
             mssparkutils.fs.rm(folder, True)
             print(f"Removed empty folder: {source_lakehouse_name}/Files/{folder.split('/Files/')[1]}")
-
 duration_secs = (datetime.now() - batch_start_time).total_seconds()
 print (f"Total Batch Time: {int(duration_secs // 60)} min and {int(duration_secs % 60)} sec. {len(files)} files. {duration_secs/(len(files) if len(files) else 1)} seconds per file.")
 #Cleanup
 sql_connection.close()
-
 
 # METADATA ********************
 
